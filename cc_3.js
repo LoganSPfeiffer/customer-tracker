639ddcb1a4785ceeeea6909c6703ba889b7f99f4
Logan Pfeiffer <Lpfeiffer48@usf.edu>
2026-02-07 16:59:57 -0500

    Created initial customer records

---
+++ cc_3.js
@@ -0,0 +1,18 @@
+let customers = [
+    {
+        name: "Hilliary Duck",
+        email: "HilliaryDuck@Gmail.com",
+        purchases: ["Iphone","AirPods","TypeCCable"]
+    },
+    {
+        name: "Jeffery Mouse",
+        email: "JefferyMouse@Gmail.com",
+        purchases: ["Apple Watch", "iPad"]
+    },
+    {
+        name: "Bill Duck",
+        email: "BillDuck@Gmail.com",
+        purchases: ["MacMini", "Magic Mouse"]
+    }
+];
+
